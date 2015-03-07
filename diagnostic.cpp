664bb38c63bab3c99e7ea4c52592d9066ba828d1
LarryTheKing <lartking@gmail.com>
2015-03-06 22:24:01 -0500

    Added !!!RESET ALL!!! and Rebuild Robot

--- diagnostic.cpp
+++ diagnostic.cpp
@@ -1,8 +1,10 @@
 #include "diagnostic.h"
 #include "ui.h"
 #include "global.h"
+#include "robot.h"
 
 #include <FEHBuzzer.h>
+#include <new>
 
 namespace G8 {
 
@@ -18,27 +20,39 @@ TASK_RESULT RedfineConstants(Robot * const pRob)
 {
     FEHBuzzer buzz;
 
-    char const ** ppNames = new char const *[CONST.GetLength()]; // Ignore first constant; add one for Quit
+    const size_t length = CONST.GetLength() + 1;
+    char const ** ppNames = new char const *[length]; // Ignore first constant; add one for Quit
     ppNames[0] = "Quit";
+    ppNames[length - 1] = "!!!RESET ALL!!!";
     for(int i = 1; i < CONST.GetLength(); i++)
     {
         ppNames[i] = CONST.GetName(i);
     }
 
-    while(size_t index = UI::MenuSelect("Select a constant", ppNames, CONST.GetLength()))
+    while(size_t index = UI::MenuSelect("Select a constant", ppNames, length))
     {
-        index;
-        switch(CONST.GetType(index))
+        if(index == length -1)
         {
-        case C_TYPE_INT:
-            CONST.Redefine<int>(index, UI::GetInt(CONST.GetName(index), CONST.GetVal<int>(index)));
-            break;
-        case C_TYPE_INT | C_TYPE_UNSIGNED:
-            CONST.Redefine<unsigned int>(index, UI::GetIntU(CONST.GetName(index), CONST.GetVal<unsigned int>(index)));
-            break;
-        default:
-            buzz.Beep();
-            break;
+            // Delete and rebuild constants
+            CONST.~CONSTANT_SYS();
+            new (&CONST) CONSTANT_SYS();
+            DefineConstants();
+            // Show message
+            LCD.WriteLine("All CONSTANTS reset!");
+            Sleep(2000);
+        }else{
+            switch(CONST.GetType(index))
+            {
+            case C_TYPE_INT:
+                CONST.Redefine<int>(index, UI::GetInt(CONST.GetName(index), CONST.GetVal<int>(index)));
+                break;
+            case C_TYPE_INT | C_TYPE_UNSIGNED:
+                CONST.Redefine<unsigned int>(index, UI::GetIntU(CONST.GetName(index), CONST.GetVal<unsigned int>(index)));
+                break;
+            default:
+                buzz.Beep();
+                break;
+            }
         }
     }
 
@@ -47,11 +61,28 @@ TASK_RESULT RedfineConstants(Robot * const pRob)
     return TASK_RESULT_SUCCESS;
 }
 
+TASK_RESULT RebuildRobot(Robot * const pRob)
+{
+    pRob->~Robot();
+
+    if(new (pRob) Robot())
+    {
+        LCD.WriteLine("Rebuild successful");
+        Sleep(2000);
+        return TASK_RESULT_SUCCESS;
+    }
+
+    LCD.WriteLine("Rebuild failed");
+    Sleep(2000);
+    return TASK_RESULT_FAIL;
+}
+
 TASK_RESULT Diagnostics(Robot * const pRob)
 {
     TaskSystem sys("Choose a diagnostic");
     sys.AddTask("Quit", Quit);
     sys.AddTask("Redefine constants", RedfineConstants);
+    sys.AddTask("Rebuild Robot", RebuildRobot);
 
     while(true)
     {
